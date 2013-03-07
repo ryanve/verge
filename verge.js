6f6f30b6913b7c6f8ded1ca08774731e978d4ddc
Ryan Van Etten <ryanve@gmail.com>
2013-03-07 17:31:54 -0500

    Enforce coding style. Tersen comments.

--- verge.js
+++ verge.js
@@ -11,13 +11,15 @@
 , nomen: true, plusplus: true, regexp: true, undef: true, sloppy: true, stupid: true
 , sub: true, white: true, indent: 4, maxerr: 180 */
 
-(function (root, name, definition) {// github.com/umdjs/umd
+(function(root, name, definition) {// github.com/umdjs/umd
     if ( typeof module != 'undefined' && module.exports ) {
         module.exports = definition(); // common|node|ender
     } else { root[name] = definition(); } // browser
-}(this, 'verge', function () {
+}(this, 'verge', function() {
 
-    var win = window
+    var viewportW
+      , viewportH
+      , win = window
       , docElem = document.documentElement
       , Modernizr = win['Modernizr']
       , matchMedia = win['matchMedia'] || win['msMatchMedia']
@@ -27,8 +29,6 @@
       , mq = Modernizr && Modernizr['mq'] || function(mq) {
             return !!media(mq).matches;
         }
-      , viewportW
-      , viewportH
       , xports = {}
       , effins = {};
     
@@ -61,7 +61,7 @@
      * @link           quirksmode.org/mobile/viewports2.html
      * @return         {number}
      */
-    xports['viewportH'] = viewportH = (function (win, docElem, mq) {
+    xports['viewportH'] = viewportH = (function(win, docElem, mq) {
         var inner = win['innerHeight'];
         return inner > docElem['clientHeight'] && mq('(min-height:' + inner + 'px)') ? function() {
             return win['innerHeight']; 
@@ -90,28 +90,25 @@
     }
     xports['scrollY'] = scrollY;
 
-    // The #verge is the amount of pixels to act as a cushion around the viewport. It can 
-    // be any number. If verge is zero, then the inX/inY/inViewport methods are exact. If 
-    // verge is set to 100, then those methods return true when for elements that are are 
-    // in the viewport *or* near it, with *near* being defined as within 100 pixels outside 
-    // the viewport edge. Elements immediately outside the viewport are 'on the verge' of 
-    // being scrolled to.
-    
+    // The #verge is the amount of pixels to act as a cushion around the viewport. It can be
+    // any number. If the verge is zero, then the inX/inY/inViewport methods are exact. If it
+    // is 100, then those methods return true when for elements that are are in the viewport 
+    // *or* near it, w/ *near* being defined as w/in 100 pixels outside the viewport edge.
+    // Elems immediately outside the viewport are 'on the verge' of being scrolled to.
+
     /** 
-     * $.rectangle()                   cross-browser element.getBoundingClientRect with an 
-     *                                 optional verge parameter. (see #verge) The coords
-     *                                 provided by the rectangle are relative to the top-left
-     *                                 corner of the viewport.
-     * @since   1.0.0 
-     * @param   {Object|Array} el      native element or matched set (defaults to first elem)
-     * @param   {number=}      verge   see #verge
-     * @param   {*=}           nix     if `nix` is truthy, the `verge` amount resets to 0. The 
-     *                                 purpose of this is so that you can use $.rectangle more 
-     *                                 easily with iterators that use the v/i/o signature.
-     * 
-     * @return  {Object|undefined}     object containing coords (`undefined` if `el` is invalid)
+     * $.rectangle()                 cross-browser element.getBoundingClientRect w/ optional 
+     *                               verge parameter. (see #verge) Coords given by rectangle
+     *                               are relative to the top-left corner of the viewport.
+     * @since  1.0.0
+     * @param  {Object|Array} el     native element or matched set (defaults to first elem)
+     * @param  {number=}      verge  see #verge
+     * @param  {*=}           nix    if `nix` is truthy, the `verge` amount resets to 0. The 
+     *                               purpose of this is so that you can use $.rectangle more 
+     *                               easily with iterators that use the v/i/o signature.
+     * @return {Object|undefined}    object containing coords (`undefined` if `el` is invalid)
      */
-    function rectangle (el, verge, nix) {
+    function rectangle(el, verge, nix) {
         var r, o;
         el = el && (el.nodeType ? el : el[0]); // isolate node
         if ( el && 1 === el.nodeType ) {
@@ -128,7 +125,7 @@
         return o;
     }
     xports['rectangle'] = rectangle;
-    effins['rectangle'] = function (verge) {
+    effins['rectangle'] = function(verge) {
         return rectangle(this, verge);
     };
 
@@ -140,7 +137,7 @@
      * @param   {number=}  verge
      * @return  {boolean}
      */
-    function inX (el, verge) {
+    function inX(el, verge) {
         var r = rectangle(el, verge);
         return !!r && r.right >= 0 && r.left <= viewportW();
     }
@@ -154,7 +151,7 @@
      * @param   {number=}  verge
      * @return  {boolean}
      */
-    function inY (el, verge) {
+    function inY(el, verge) {
         var r = rectangle(el, verge);
         return !!r && r.bottom >= 0 && r.top <= viewportH();
     }
@@ -167,7 +164,7 @@
      * @param   {number=}  verge
      * @return  {boolean}
      */
-    function inViewport (el, verge) {
+    function inViewport(el, verge) {
         // Equiv to `inX(el, verge) && inY(el, verge)` but just manually do both 
         // to avoid calling rectangle() twice. It gzips just as small like this.
         var r = rectangle(el, verge);
